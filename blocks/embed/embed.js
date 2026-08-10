2d6a03b005a2f83f8edcae35595dc8861d4c61b4
naveenkambam-codeandtheory <naveen.kambam@codeandtheory.com>
2026-08-10 10:23:17 +0530

    Added Block Collection

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -1,73 +1,113 @@
-function getInstagramEmbedUrl(inputUrl) {
-  try {
-    const url = new URL(inputUrl);
-    const host = url.hostname.toLowerCase();
-    const isInstagramHost = host === 'instagram.com'
-      || host === 'www.instagram.com'
-      || host === 'instagr.am';
+/*
+ * Embed Block
+ * Show videos and social posts directly on your page
+ * https://www.hlx.live/developer/block-collection/embed
+ */
 
-    if (!isInstagramHost) return null;
-
-    const pathMatch = url.pathname.match(/^\/(p|reel|tv)\/[^/]+/i);
-    if (!pathMatch) return null;
-
-    return url.toString().replace(/\/$/, '');
-  } catch (error) {
-    return null;
+const loadScript = (url, callback, type) => {
+  const head = document.querySelector('head');
+  const script = document.createElement('script');
+  script.src = url;
+  if (type) {
+    script.setAttribute('type', type);
   }
-}
+  script.onload = callback;
+  head.append(script);
+  return script;
+};
 
-function loadInstagramEmbedScript() {
-  const existingScript = document.querySelector('script[src="https://www.instagram.com/embed.js"]');
-  if (existingScript) {
-    if (window.instgrm?.Embeds?.process) {
-      window.instgrm.Embeds.process();
-    }
+const getDefaultEmbed = (url) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+    <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
+      scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
+    </iframe>
+  </div>`;
+
+const embedYoutube = (url, autoplay) => {
+  const usp = new URLSearchParams(url.search);
+  const suffix = autoplay ? '&muted=1&autoplay=1' : '';
+  let vid = usp.get('v') ? encodeURIComponent(usp.get('v')) : '';
+  const embed = url.pathname;
+  if (url.origin.includes('youtu.be')) {
+    [, vid] = url.pathname.split('/');
+  }
+  const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+      <iframe src="https://www.youtube.com${vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+      allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" allowfullscreen="" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
+    </div>`;
+  return embedHTML;
+};
+
+const embedVimeo = (url, autoplay) => {
+  const [, video] = url.pathname.split('/');
+  const suffix = autoplay ? '?muted=1&autoplay=1' : '';
+  const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+      <iframe src="https://player.vimeo.com/video/${video}${suffix}" 
+      style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+      frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen  
+      title="Content from Vimeo" loading="lazy"></iframe>
+    </div>`;
+  return embedHTML;
+};
+
+const embedTwitter = (url) => {
+  const embedHTML = `<blockquote class="twitter-tweet"><a href="${url.href}"></a></blockquote>`;
+  loadScript('https://platform.twitter.com/widgets.js');
+  return embedHTML;
+};
+
+const loadEmbed = (block, link, autoplay) => {
+  if (block.classList.contains('embed-is-loaded')) {
     return;
   }
 
-  const script = document.createElement('script');
-  script.async = true;
-  script.src = 'https://www.instagram.com/embed.js';
-  script.onload = () => {
-    if (window.instgrm?.Embeds?.process) {
-      window.instgrm.Embeds.process();
-    }
-  };
-  document.head.append(script);
-}
+  const EMBEDS_CONFIG = [
+    {
+      match: ['youtube', 'youtu.be'],
+      embed: embedYoutube,
+    },
+    {
+      match: ['vimeo'],
+      embed: embedVimeo,
+    },
+    {
+      match: ['twitter'],
+      embed: embedTwitter,
+    },
+  ];
 
-function buildFallbackLink(url) {
-  const link = document.createElement('a');
-  link.href = url;
-  link.textContent = 'Open Instagram post';
-  link.target = '_blank';
-  link.rel = 'noreferrer noopener';
-  link.className = 'embed-link';
-  return link;
-}
+  const config = EMBEDS_CONFIG.find((e) => e.match.some((match) => link.includes(match)));
+  const url = new URL(link);
+  if (config) {
+    block.innerHTML = config.embed(url, autoplay);
+    block.classList = `block embed embed-${config.match[0]}`;
+  } else {
+    block.innerHTML = getDefaultEmbed(url);
+    block.classList = 'block embed';
+  }
+  block.classList.add('embed-is-loaded');
+};
 
 export default function decorate(block) {
-  const sourceLink = block.querySelector('a[href]');
-  const embedUrl = sourceLink ? getInstagramEmbedUrl(sourceLink.href) : null;
+  const placeholder = block.querySelector('picture');
+  const link = block.querySelector('a').href;
+  block.textContent = '';
 
-  block.classList.add('embed-instagram');
-
-  const frameWrapper = document.createElement('div');
-  frameWrapper.className = 'embed-frame';
-
-  if (embedUrl) {
-    const quote = document.createElement('blockquote');
-    quote.className = 'instagram-media';
-    quote.setAttribute('data-instgrm-permalink', embedUrl);
-    quote.setAttribute('data-instgrm-version', '14');
-    quote.setAttribute('data-instgrm-captioned', 'true');
-    frameWrapper.append(quote);
-    loadInstagramEmbedScript();
+  if (placeholder) {
+    const wrapper = document.createElement('div');
+    wrapper.className = 'embed-placeholder';
+    wrapper.innerHTML = '<div class="embed-placeholder-play"><button type="button" title="Play"></button></div>';
+    wrapper.prepend(placeholder);
+    wrapper.addEventListener('click', () => {
+      loadEmbed(block, link, true);
+    });
+    block.append(wrapper);
   } else {
-    const fallback = buildFallbackLink(sourceLink?.href || '#');
-    frameWrapper.append(fallback);
+    const observer = new IntersectionObserver((entries) => {
+      if (entries.some((e) => e.isIntersecting)) {
+        observer.disconnect();
+        loadEmbed(block, link);
+      }
+    });
+    observer.observe(block);
   }
-
-  block.replaceChildren(frameWrapper);
 }
--- scripts/consent-check.js
+++ scripts/consent-check.js
@@ -44,3 +44,37 @@ function onConsentUpdate() {
 }
 
 onConsentUpdate();
+
+// scripts/consent-check.js
+// const ONETRUST_SRC = 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js';
+// const ONETRUST_DATA_DOMAIN = 'your-onetrust-domain-id';
+
+// let consentedLoaded = false;
+
+// function loadConsented() {
+//   if (consentedLoaded) return;
+//   consentedLoaded = true;
+//   import('./consented.js');
+// }
+
+// // OneTrust calls this whenever the banner initializes or preferences change
+// window.OptanonWrapper = () => {
+//   const activeGroups = window.OnetrustActiveGroups || '';
+//   // adjust to the categories your project actually needs
+//   const consented = activeGroups.includes('C0004');
+//   window.dispatchEvent(new CustomEvent('consent.update', { detail: { consented } }));
+//   if (consented) {
+//     loadConsented();
+//   }
+// };
+
+// function loadOneTrust() {
+//   const script = document.createElement('script');
+//   script.src = ONETRUST_SRC;
+//   script.type = 'text/javascript';
+//   script.charset = 'UTF-8';
+//   script.setAttribute('data-domain-script', ONETRUST_DATA_DOMAIN);
+//   document.head.append(script);
+// }
+
+// loadOneTrust();
